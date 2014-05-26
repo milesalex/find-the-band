214cf0e3e4516848e35774e5721d7b81d80b8603
Alex Miles <howdy@alexmilesdesign.com>
2014-05-26 00:36:01 -0700

    Clean up code

--- search.js
+++ search.js
@@ -1,8 +1,8 @@
 function onBandClick(info, tab) {
-  console.log("item " + info.menuItemId + " was clicked");
-  console.log("info: " + JSON.stringify(info));
-  console.log("tab: " + JSON.stringify(tab));
-  console.log("**************")
+  // console.log("item " + info.menuItemId + " was clicked");
+  // console.log("info: " + JSON.stringify(info));
+  // console.log("tab: " + JSON.stringify(tab));
+  // console.log("**************")
 
   var name = JSON.stringify(info.selectionText);
   name = name.substring(1, name.length-1) + ' band';
@@ -16,11 +16,10 @@ function onBandClick(info, tab) {
   chrome.tabs.create({ url: newURL });
 }
 
-// Create one test item for each context type.
 var context = "link";
 var title = "Find the band";
 var id = chrome.contextMenus.create({
   "title": title,
-  "contexts":[context],
+  "contexts": [context],
   "onclick": onBandClick
 });
